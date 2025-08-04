80ffc91fe808ba3c9e75d3d42944ddb1aa04b993
stpl-Pramod <pramod.bhamare@shauryatechnosoft.com>
2025-08-04 23:45:04 +0530

    tractor changes

--- src/app/pages/tractors/tractors.ts
+++ src/app/pages/tractors/tractors.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { Tractor } from '../../core/services/tractor';
+import {MatTableModule} from '@angular/material/table';
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
+
+
 
 @Component({
   selector: 'app-tractors',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './tractors.html',
   styleUrl: './tractors.scss'
 })
 export class Tractors {
+  displayedColumns: string[] = ['tractor_id', 'owner_id', 'brand', 'model', 'year', 'location', 'price_per_day', 'status'];
+  dataSource :any;
 
   constructor(private tractorApi:Tractor){
 
@@ -18,7 +30,10 @@ export class Tractors {
   
   getTractors(){
   this.tractorApi.getAll().subscribe({
-      next: (res:any) => console.log('Tractors22:', res),
+      next: (res:any) => {
+        console.log('Tractors22:', res);
+        this.dataSource = res;
+      },
       error: (err:any) => console.error('Error222:', err)
     });
   }
